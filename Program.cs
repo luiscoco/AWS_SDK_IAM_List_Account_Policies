fae2e97173c25787dd59e28a1f3eafefa170d261
Luis Coco Enríquez <LEnriquez@luxoft.com>
2025-04-01 21:43:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+
+namespace IAMActions;
+
+public class IAM_List_Account_Policites
+{
+    static async Task Main(string[] args)
+    {
+        // Getting started with AWS Identity and Access Management (IAM). List
+        // the policies for the account.
+        var iamClient = new AmazonIdentityManagementServiceClient();
+
+        var listPoliciesPaginator = iamClient.Paginators.ListPolicies(new ListPoliciesRequest());
+        var policies = new List<ManagedPolicy>();
+
+        await foreach (var response in listPoliciesPaginator.Responses)
+        {
+            policies.AddRange(response.Policies);
+        }
+
+        Console.WriteLine("Here are the policies defined for your account:\n");
+        policies.ForEach(policy =>
+        {
+            Console.WriteLine($"Created: {policy.CreateDate}\t{policy.PolicyName}\t{policy.Description}");
+        });
+    }
+}
